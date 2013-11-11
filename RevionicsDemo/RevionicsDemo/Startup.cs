5ddfaf1a5118b6bbce44e0fe6be170570aef2d12
Ravi Gadag <rjgadag@gmail.com>
2013-11-11 23:17:21 +0530

    Intial commit

---
+++ RevionicsDemo/RevionicsDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RevionicsDemo.Startup))]
+namespace RevionicsDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
